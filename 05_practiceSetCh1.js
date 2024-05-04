b3d0220ab24d8915c19e702677ddeb7024acb5e5
SNK128 <sagniknayak129@gmail.com>
2024-05-04 15:33:29 +0530

    5th lec added

---
+++ 05_practiceSetCh1.js
@@ -0,0 +1,35 @@
+//Q1
+
+let a = "Sagnik";
+let b = 6;
+console.log(a + b);
+
+//Q2
+console.log(typeof (a + b));
+
+//Q3
+const a1 = {
+  name: "Sagnik",
+  section: 1,
+  isPrinciple: false,
+};
+
+// a1=9  a1={} will throw an error .can't use a1 to redefine as number/string/obj etc
+
+//Q4
+a1["name"] = "Lovish";
+a1["friend"] = "Shubham";
+
+console.log(a1);
+
+//Q5
+
+const dict = {
+  appreciate: "recognize the full worth of",
+  hello: "say hi",
+  bye: "Goodbye",
+};
+
+console.log(dict);
+console.log(dict.hello);
+console.log(dict["appreciate"]);
