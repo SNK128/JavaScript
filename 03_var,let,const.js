a2a1adbc43c858ab23c4fda7cd2311f53b0ca4da
SNK128 <sagniknayak129@gmail.com>
2024-05-04 15:16:05 +0530

    3rd lec added for var,let,const

---
+++ 03_var,let,const.js
@@ -0,0 +1,38 @@
+console.log("javatutorial lec 3 var,let,const")
+
+var a=45
+var a="p"
+
+console.log(a)
+
+let b="harry"
+const c="hi"
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+b=67
+//c=21  //will throw error bcuz c is constant variable
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+//let b="potter"  //let can be updated but not redeclated
+
+{
+  var a=100
+  let b="hello"
+  
+
+  console.log(a)
+  console.log(b)
+  console.log(c)
+}
+
+console.log("///////////////")
+
+console.log(a)
+console.log(b)
+console.log(c)
