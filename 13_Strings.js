4551379ef8de5c1e80e1ccdb8b00956f9c7f6f2a
SNK128 <sagniknayak129@gmail.com>
2024-05-13 01:28:35 +0530

    lec 13 added

---
+++ 13_Strings.js
@@ -0,0 +1,20 @@
+let n="Sagnik"
+let nickname='Sunny'
+
+console.log(n,nickname)
+console.log(n[0])
+console.log(n.length)
+
+//Template literals
+let boy1="Pramod"
+let boy2="Nikhil"
+let sentence=`boy2 is a friend of boy1`
+console.log(sentence)
+sentence =`${boy2} is 'a'  friend of ${boy1}`
+console.log(sentence)
+
+
+//Escape Sequence Characters
+let fruit='Bana\'na'
+console.log(fruit)
+console.log(fruit.length)
