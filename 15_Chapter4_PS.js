cf0dac9bc675126e95ec2c61e329098b5802a333
SNK128 <sagniknayak129@gmail.com>
2024-05-13 02:42:13 +0530

    15th lec added

---
+++ 15_Chapter4_PS.js
@@ -0,0 +1,27 @@
+//Q1
+console.log("har\"".length)
+
+//Q2 Explore 'include'
+const sentence='The quick brown fox jumps over the lazy dog'
+const word='fox'
+const word2='fox1'
+console.log(sentence.includes(word)) //rturns boolean
+console.log(`The word "${word}" ${sentence.includes(word)?'is' : 'is not'} in the sentence`);
+console.log(`The word "${word2}" ${sentence.includes(word2)?'is' : 'is not'} in the sentence`);
+//startsWith
+console.log(sentence.startsWith("The"))
+console.log(sentence.endsWith("The"))
+
+//Q3
+console.log(sentence.toLowerCase())
+
+//Q4 Extract number from string
+let str="Please give Rs 1000"
+let amount=Number.parseInt(str.slice("Please give Rs ".length))
+console.log(amount)
+
+//Q5 try to change character of a string
+
+let b='kkkkkk'
+b[0]='a'
+console.log(b)
